9f80b50bf326ed28bb8ae4ab0482f0d93ec743e9
Agnis "NeZvērs" Aldiņš <agnis16@gmail.com>
2021-08-13 14:54:13 +0200

    Update main.c

--- HitboxCollision/Example/main.c
+++ HitboxCollision/Example/main.c
@@ -14,13 +14,13 @@ void GameUpdate();
 void GameDraw();
 
 #define SOLID_COUNT 4
-Hitbox solids[] = {
+NezRect_f solids[] = {
     {200.0f, 200.0f, 16.0f, 16.0f},
     {216.0f, 184.0f, 16.0f, 16.0f},
     {224.0f, 168.0f, 16.0f, 16.0f},
     {232.0f, 152.0f, 16.0f, 16.0f},
 };
-Hitbox player = {100.0f, 100.0f, 16.0, 16.0f};
+NezRect_f player = {100.0f, 100.0f, 16.0, 16.0f};
 
 float spdX = 0.0f;
 float spdY = 0.0f;
@@ -28,13 +28,23 @@ float spdY = 0.0f;
 int main(void)
 {
     GameInit();
+    
+
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         GameUpdate();
         GameDraw();
     }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
     return 0;
 }
 
