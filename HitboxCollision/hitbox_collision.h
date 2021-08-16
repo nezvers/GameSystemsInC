5779c6275f17b3992ef371b2e04f7fa707216cc3
Agnis "NeZvērs" Aldiņš <agnis16@gmail.com>
2021-08-16 15:51:26 +0300

    Add files via upload

--- HitboxCollision/hitbox_collision.h
+++ HitboxCollision/hitbox_collision.h
@@ -5,6 +5,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "stdbool.h"
+#include "math.h"
 
 #ifndef NEZRECT_F
 #define NEZRECT_F
@@ -71,6 +72,41 @@ void HitboxMoveAndCollideHitbox(NezRect_f *a, NezRect_f *b, float *spdX, float *
         else if (*spdY < 0.0f) {
             *spdY = (b->y + b->h) - a->y;
         }
+    }
+}
+
+// Test if movement direction can result with collision
+bool HitboxMoveCollisionCheck(NezRect_f *a, NezRect_f *b, float *spdX, float *spdY) {
+    float d = (float)sqrt(*spdX * *spdX + *spdY * *spdY);
+    float projX = *spdX / d;
+    float projY = *spdY / d;
+    
+    float min_r1 = INFINITY;
+    float max_r1 = -INFINITY;
+    float a0 = ((a->x) * projX + a->y * (projY));
+    float a1 = ((a->x + a->w) * projX + a->y * projY);
+    float a2 = ((a->x + a->w) * projX + (a->y + a->h) * projY);
+    float a3 = ((a->x) * projX + (a->y + a->h) * projY);
+    min_r1 = fminf(min_r1, a0); max_r1 = fmaxf(max_r1, a0);
+    min_r1 = fminf(min_r1, a1); max_r1 = fmaxf(max_r1, a1);
+    min_r1 = fminf(min_r1, a2); max_r1 = fmaxf(max_r1, a2);
+    min_r1 = fminf(min_r1, a3); max_r1 = fmaxf(max_r1, a3);
+    
+    float min_r2 = INFINITY;
+    float max_r2 = -INFINITY;
+    float b0 = ((b->x) * projX + b->y * (projY));
+    float b1 = ((b->x + b->w) * projX + b->y * projY);
+    float b2 = ((b->x + b->w) * projX + (b->y + b->h) * projY);
+    float b3 = ((b->x) * projX + (b->y + b->h) * projY);
+    min_r2 = fminf(min_r2, b0); max_r2 = fmaxf(max_r2, b0);
+    min_r2 = fminf(min_r2, b1); max_r2 = fmaxf(max_r2, b1);
+    min_r2 = fminf(min_r2, b2); max_r2 = fmaxf(max_r2, b2);
+    min_r2 = fminf(min_r2, b3); max_r2 = fmaxf(max_r2, b3);
+    
+    if (!(max_r2 >= min_r1 && max_r1 >= min_r2)){
+        return false;
+    }else{
+        return true;
     }
 }
 
