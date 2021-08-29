5339648fa1fb810bf0ad01ee0b1bd346bc7c2c57
nezvers <agnis16@gmail.com>
2021-08-30 01:04:27 +0300

    Reworking for rectangle list

--- HitboxCollision/Example/main.c
+++ HitboxCollision/Example/main.c
@@ -6,6 +6,7 @@
 #define HITBOX_COLLISIONS_IMPLEMENTATION
 #include "hitbox_collision.h"
 
+
 const int screenWidth = 800;
 const int screenHeight = 600;
 
@@ -13,6 +14,11 @@ void GameInit();
 void GameUpdate();
 void GameDraw();
 
+void DrawTileGrid();
+void DrawTileMap();
+
+#define MAP_W 20
+#define MAP_H 12
 #define SOLID_COUNT 6
 NezRect_f solids[] = {
     {192.0f, 454.0f, 96.0f, 32.0f},
@@ -22,9 +28,23 @@ NezRect_f solids[] = {
     {-18.0f, 0.0f, 32.0f, 600.0f},// left wall
     {782.0f, 0.0f, 32.0f, 600.0f},// right wall
 };
-NezRect_f player = {100.0f, 100.0f, 32.0, 32.0f};
+NezRect_f player = {32.0f * 2, 32.0f * 8, 32.0f, 32.0f};
 
-
+NezGridMap map;
+int tiles[] = {
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+};
 
 int main(void){
     GameInit();
@@ -39,6 +59,12 @@ int main(void){
 
 void GameInit() {
     InitWindow(screenWidth, screenHeight, "raylib [models] example - geometric shapes");
+    map.x = 0.0f;
+    map.y = 0.0f;
+    map.w = MAP_W;
+    map.h = MAP_H;
+    map.s = 32;
+    map.cell = tiles;
 }
 
 
@@ -48,8 +74,7 @@ void GameUpdate(){
     static const float jumpSpd = -10.0f;
     static const float grav = 0.4f;
     static const float acc = 0.1;
-    static float spdX = 0.0f;
-    static float spdY = 0.0f;
+    static NezVec2_f vel = {0};
     static bool isGrounded = false;
     static int dirX = 0;
     
@@ -57,33 +82,33 @@ void GameUpdate(){
     dirX = (float)(IsKeyDown(KEY_D) - IsKeyDown(KEY_A));
     
     // HORIZONTAL SPEED
-    spdX += (dirX * maxX - spdX) * acc;
-    if (spdX < -maxX){
-        spdX = -maxX;
+    vel.x += (dirX * maxX - vel.x) * acc;
+    if (vel.x < -maxX){
+        vel.x = -maxX;
     }
-    else if (spdX > maxX){
-        spdX = maxX;
+    else if (vel.x > maxX){
+        vel.x = maxX;
     }
     
     // VERTICAL SPEED
     if (isGrounded && IsKeyPressed(KEY_SPACE)){
-        spdY = jumpSpd;
+        vel.y = jumpSpd;
     }
     else{
-        spdY += grav;
+        vel.y += grav;
     }
-    if (spdY > -jumpSpd){
-        spdY = -jumpSpd;
+    if (vel.y > -jumpSpd){
+        vel.y = -jumpSpd;
     }
-    float prevY = spdY;
+    float prevY = vel.y;
     
     // COLLISION
-    for (int i = 0; i < SOLID_COUNT; i++){
-        HitboxMoveAndCollideHitbox(&player, &solids[i], &spdX, &spdY);
-    }
+    //HitboxMoveAndCollide(&player, &vel, solids, SOLID_COUNT);
+    HitboxMoveAndCollideGrid(&player, &vel, &map);
     
     // GROUND CHECK
-    if (prevY > 0.0f && spdY <= 0.0f){
+    printf("%f, %f\n", prevY, vel.y);
+    if (prevY > vel.y){
         isGrounded = true;
     }
     else{
@@ -91,8 +116,9 @@ void GameUpdate(){
     }
     
     // POSITION
-    player.x += spdX;
-    player.y += spdY;
+    if(player.y > 32 * 11){
+        player.y = 32 * 11;
+    }
 }
 
 
@@ -100,13 +126,48 @@ void GameDraw(){
     BeginDrawing();
     ClearBackground(RAYWHITE);
     
+    DrawTileMap();
+    DrawTileGrid();
     // PLAYER
     DrawRectangle((int)player.x, (int)player.y, (int)player.w, (int)player.h, BLUE);
-    
-    // SOLIDS
-    for (int i = 0; i < SOLID_COUNT; i++) {
-        DrawRectangle((int)solids[i].x, (int)solids[i].y, (int)solids[i].w, (int)solids[i].h, RED);
-    }
 
     EndDrawing();
+    
 }
+
+void DrawTileMap(){
+    for (int y = 0; y < map.h; y++){
+        for (int x = 0; x < map.w; x++){
+            int i = x + y * map.w;
+            int tile = map.cell[i];
+            if (tile){
+                float cellX = (map.x + map.s * x);
+                float cellY = (map.y + map.s * y);
+                DrawRectangle((int)cellX, (int)cellY, map.s, map.s, LIME);
+                // check tile above
+                if (i - map.w >= 0 && !map.cell[i - map.w]){
+                    DrawLineEx((Vector2){cellX, cellY + 3}, (Vector2){cellX + map.s, cellY + 3}, 6.0f, GREEN);
+                }
+            }
+        }
+    }
+}
+
+void DrawTileGrid(){
+    Color c = (Color){255,255,255,25};
+    
+    for (int y = 0; y < map.h + 1; y++){
+        int x1 = map.x;
+        int x2 = map.x + map.w * map.s;
+        int Y = map.y + map.s * y;
+        DrawLine(x1, Y, x2, Y, c);
+    }
+    for (int x = 0; x < map.w + 1; x++){
+        int y1 = map.y;
+        int y2 = map.y + map.h * map.s;
+        int X = map.x + map.s * x;
+        DrawLine(X, y1, X, y2, c);
+    }
+}
+
+
--- HitboxCollision/hitbox_collision.h
+++ HitboxCollision/hitbox_collision.h
@@ -31,6 +31,21 @@ typedef struct {
 } NezVec2_f;
 #endif // NEZVEC2_F
 
+
+//tile collision types
+#ifndef NEZGRIDMAP
+#define NEZGRIDMAP
+typedef struct{
+    float x;
+    float y;
+    int w;      // width
+    int h;      // height
+    int s;      // cell size (square cells)
+    int *cell;  // array of tiles
+}NezGridMap;
+#endif // NEZGRIDMAP
+
+
 #ifndef NEZHBAPI
     #ifdef NEZ_HITBOX_STATIC
         #define NEZHBAPI static
@@ -48,19 +63,13 @@ extern "C" {
         HitboxCheckHitbox(NezRect_f *a, NezRect_f *b);
     // Get remaining move distance after collision check. Internally calls HitboxCheckHitbox()
     NEZHBAPI void
-        HitboxMoveAndCollideHitbox(NezRect_f *a, NezRect_f *b, float *spdX, float *spdY);
-    // Check if movement axis can result in collision. Using Separated Axis Theorem.
-    NEZHBAPI bool
-        HitboxAxisCollisionCheck(NezRect_f *a, NezRect_f *b, float *dirX, float *dirY);
-    // Raycast against rectangle
-    NEZHBAPI bool
-        HitboxVsRaycast(NezVec2_f *rayOrig, NezVec2_f *rayDir, NezRect_f *target, NezVec2_f *colPoint, NezVec2_f *colNormal, float *timeStep);
-    // Dynamic rectangle vs rectangle collision. Velocity gets multiplied by deltaTime. Pass deltaTime value of 1.0f if full velocity length should be performed.
-    NEZHBAPI bool
-        HitboxDynamicVsHitbox(NezRect_f *movingRect, NezVec2_f *velocity, NezRect_f *target, NezVec2_f *colPoint, NezVec2_f *colNormal, float *timeStep);
-    // Resolve movement collision between rectangles
-    NEZHBAPI bool
-        HitboxResolveDynamicRectVsRect(NezRect_f *movingRect, NezVec2_f *velocity, NezRect_f *target);
+        HitboxMoveAndCollide(NezRect_f *a, NezVec2_f *velocity, NezRect_f *b, int count);
+    NEZHBAPI NezRect_f
+        HitboxExpand(NezRect_f *rect, NezVec2_f *size);
+    NEZHBAPI void
+        HitboxListFromGrid(NezRect_f *rect, NezGridMap *grid, NezRect_f **outList, int *outCount);
+    NEZHBAPI void
+        HitboxMoveAndCollideGrid(NezRect_f *a, NezVec2_f *velocity, NezGridMap *grid);
     
 #ifdef __cplusplus
 }
@@ -77,162 +86,102 @@ static inline void swapf(float *a, float *b){
    *a = t;
 }
 
-bool HitboxCheckHitbox(NezRect_f *a, NezRect_f *b) {
+bool HitboxCheckHitbox(NezRect_f *a, NezRect_f *b){
     return  a->x < b->x + b->w && a->x + a->w > b->x && a->y< b->y + b->h && a->y + a->h > b->y;
 }
 
 
-void HitboxMoveAndCollideHitbox(NezRect_f *a, NezRect_f *b, float *spdX, float *spdY) {
-    NezRect_f c = (NezRect_f){a->x + *spdX, a->y, a->w, a->h};
-
-    if (HitboxCheckHitbox(&c, b)) {
-        if (*spdX > 0.0f) {
-            *spdX = (b->x - a->w) - a->x;
-        }
-        else if (*spdX < 0.0f) {
-            *spdX = (b->x + b->w) - a->x;
+void HitboxMoveAndCollide(NezRect_f *a, NezVec2_f *velocity, NezRect_f *b, int count){
+    int i;
+    bool hitX = false;
+    bool hitY = false;
+    
+    a->x += velocity->x;
+    for (i = 0; i < count; i++){
+        if (HitboxCheckHitbox(a, &b[i])) {
+            if (velocity->x > 0.0f) {
+                a->x = (b[i].x - a->w);
+                hitX = true;
+            }
+            else if (velocity->x < 0.0f) {
+                a->x = (b[i].x + b[i].w);
+                hitX = true;
+            }
         }
     }
-    c.x = a->x + *spdX;
-    c.y += *spdY;
-
-    if (HitboxCheckHitbox(&c, b)) {
-        if (*spdY > 0.0f) {
-            *spdY = (b->y - a->h) - a->y;
+    if(hitX){velocity->x = 0.0f;}
+    
+    a->y += velocity->y;
+    for (i = 0; i < count; i++){
+        if (HitboxCheckHitbox(a, &b[i])) {
+            if (velocity->y > 0.0f) {
+                a->y = (b[i].y - a->h);
+                hitY = true;
+            }
+            else if (velocity->y < 0.0f) {
+                a->y = (b[i].y + b[i].h);
+                hitY = true;
+            }
         }
-        else if (*spdY < 0.0f) {
-            *spdY = (b->y + b->h) - a->y;
+    }
+    if(hitY){velocity->y = 0.0f;}
+    printf("y: %f\n", velocity->y);
+}
+
+NezRect_f HitboxExpand(NezRect_f *rect, NezVec2_f *size){
+    return (NezRect_f){
+        size->x > 0 ? rect->x : rect->x + size->x,
+        size->y > 0 ? rect->y : rect->y + size->y,
+        size->x > 0 ? rect->w + size->x : rect->w - size->x,
+        size->y > 0 ? rect->h + size->y : rect->h - size->y
+        };
+}
+
+void HitboxListFromGrid(NezRect_f *rect, NezGridMap *grid, NezRect_f **outList, int *outCount){
+    float offX = rect->x - grid->x;
+    float offY = rect->y - grid->y;
+    // compensate flooring
+    if (offX < 0.0f){offX -= grid->s;}
+    if (offY < 0.0f){offY -= grid->s;}
+    
+    // grid coordinates
+    int X = (int)(offX / grid->s);
+    int sizeX = (int)(rect->w / grid->s) + 1;
+    int Y = (int)(offY / grid->s);
+    int sizeY = (int)(rect->h / grid->s) + 1;
+    
+    *outList = malloc(sizeof(NezRect_f) * sizeX * sizeY);
+    //printf("offX: %d, %d\n", sizeX, sizeY);
+    *outCount = 0;
+    
+    for (int y = Y; y < Y + sizeY; y++){
+        if (y >= 0 && y < grid->h){
+            for (int x = X; x < X + sizeX; x++){
+                if (x >= 0 && x < grid->w){
+                    int tile = grid->cell[x + y * grid->w];
+                    if (tile){
+                        (*outList)[*outCount] = (NezRect_f){
+                            grid->x + x * grid->s,
+                            grid->y + y * grid->s,
+                            grid->s,
+                            grid->s
+                            };
+                        *outCount += 1;
+                    }
+                }
+            }
         }
     }
 }
 
-
-bool HitboxAxisCollisionCheck(NezRect_f *a, NezRect_f *b, float *dirX, float *dirY) {
-    float d = (float)sqrt(*dirX * *dirX + *dirY * *dirY);
-    float projX = -*dirY / d;
-    float projY = *dirX / d;
-    
-    float min_r1 = INFINITY;
-    float max_r1 = -INFINITY;
-    float a0 = ((a->x) * projX + a->y * (projY));
-    float a1 = ((a->x + a->w) * projX + a->y * projY);
-    float a2 = ((a->x + a->w) * projX + (a->y + a->h) * projY);
-    float a3 = ((a->x) * projX + (a->y + a->h) * projY);
-    min_r1 = fminf(min_r1, a0); max_r1 = fmaxf(max_r1, a0);
-    min_r1 = fminf(min_r1, a1); max_r1 = fmaxf(max_r1, a1);
-    min_r1 = fminf(min_r1, a2); max_r1 = fmaxf(max_r1, a2);
-    min_r1 = fminf(min_r1, a3); max_r1 = fmaxf(max_r1, a3);
-    
-    float min_r2 = INFINITY;
-    float max_r2 = -INFINITY;
-    float b0 = ((b->x) * projX + b->y * (projY));
-    float b1 = ((b->x + b->w) * projX + b->y * projY);
-    float b2 = ((b->x + b->w) * projX + (b->y + b->h) * projY);
-    float b3 = ((b->x) * projX + (b->y + b->h) * projY);
-    min_r2 = fminf(min_r2, b0); max_r2 = fmaxf(max_r2, b0);
-    min_r2 = fminf(min_r2, b1); max_r2 = fmaxf(max_r2, b1);
-    min_r2 = fminf(min_r2, b2); max_r2 = fmaxf(max_r2, b2);
-    min_r2 = fminf(min_r2, b3); max_r2 = fmaxf(max_r2, b3);
-    
-    if (!(max_r2 >= min_r1 && max_r1 >= min_r2)){
-        return false;
-    }else{
-        return true;
-    }
-}
-
-// Ported from Javidx9 rectangle collision video
-bool HitboxVsRaycast(NezVec2_f *rayOrig, NezVec2_f *rayDir, NezRect_f *target, NezVec2_f *colPoint, NezVec2_f *colNormal, float *timeStep){
-    *colNormal = (NezVec2_f){0.0f, 0.0f};
-    *colPoint = (NezVec2_f){0.0f, 0.0f};
-    
-    // Cache division
-    NezVec2_f invdir = (NezVec2_f){1.0f / rayDir->x, 1.0f / rayDir->y};
-    
-    // Calculate intersections with rectangle bounding axes
-    NezVec2_f t_near = (NezVec2_f){(target->x - rayOrig->x) * invdir.x, (target->y - rayOrig->y) * invdir.y};
-    NezVec2_f t_far = (NezVec2_f){(target->x + target->w - rayOrig->x) * invdir.x, (target->y + target->h - rayOrig->y) * invdir.y};
-    
-    if (isnan(t_near.x) || isnan(t_near.y)){
-        return false;}
-    if (isnan(t_far.x) || isnan(t_far.y)){
-        return false;}
-    
-    // Sort distance
-    if (t_near.x > t_far.x) swapf(&t_near.x, &t_far.x);
-    if (t_near.y > t_far.y) swapf(&t_near.y, &t_far.y);
-    
-    // Early rejection
-    if (t_near.x > t_far.y || t_near.y > t_far.x) {return false;}
-    
-    // Closest 'time' will be the first contact
-    *timeStep = fmaxf(t_near.x, t_near.y);
-    
-    // Furthest 'time' is contact on opposite side of target
-    float t_hit_far = fminf(t_far.x, t_far.y);
-    
-    // Reject if ray direction is pointing away from object
-    if (t_hit_far < 0){;return false;}
-    
-    // Contact point of collision from parametric line equation
-    *colPoint = (NezVec2_f){rayOrig->x + *timeStep * rayDir->x, rayOrig->y + *timeStep * rayDir->y};
-    
-    if (t_near.x > t_near.y){
-        if (invdir.x < 0){
-            *colNormal = (NezVec2_f){ 1.0f, 0.0f };
-        }else{
-            *colNormal = (NezVec2_f){ -1.0f, 0.0f };
-        }
-    }else if (t_near.x < t_near.y){
-        if (invdir.y < 0){
-            *colNormal = (NezVec2_f){ 0.0f, 1.0f };
-        }else{
-            *colNormal = (NezVec2_f){ 0.0f, -1.0f };
-        }
-    }
-    
-    // Note if t_near == t_far, collision is principly in a diagonal
-    // so pointless to resolve. By returning a CN={0,0} even though its
-    // considered a hit, the resolver wont change anything.
-    
-    return true;
-}
-
-// Ported from Javidx9 rectangle collision video
-bool HitboxDynamicVsHitbox(NezRect_f *movingRect, NezVec2_f *velocity, NezRect_f *target, NezVec2_f *colPoint, NezVec2_f *colNormal, float *timeStep){
-    // Check if dynamic rectangle is actually moving - we assume rectangles are NOT in collision to start
-    if (velocity->x == 0.0f && velocity->y == 0.0f){return false;}
-    
-    // Expand target rectangle by source dimensions
-    NezRect_f expanded_target = (NezRect_f){
-        target->x - movingRect->w * 0.5,
-        target->y - movingRect->h * 0.5,
-        target->w + movingRect->w,
-        target->h + movingRect->h,
-    };
-    NezVec2_f rayOrigin = {movingRect->x + movingRect->w * 0.5, movingRect->y + movingRect->h * 0.5};
-    NezVec2_f rayDir = {velocity->x, velocity->y};
-    
-    
-    if (HitboxVsRaycast(&rayOrigin, &rayDir, &expanded_target, colPoint, colNormal, timeStep)){
-        return (*timeStep >= 0.0f && *timeStep < 1.0f);
-    }
-    else{
-        return false;
-    }
-}
-
-// Ported from Javidx9 rectangle collision video
-bool HitboxResolveDynamicRectVsRect(NezRect_f *movingRect, NezVec2_f *velocity, NezRect_f *target){
-    NezVec2_f colPoint;
-    NezVec2_f colNormal;
-    float timeStep = 0.0f;
-    if (HitboxDynamicVsHitbox(movingRect, velocity, target, &colPoint, &colNormal, &timeStep)){
-        velocity->x += colNormal.x * (float)fabs(velocity->x) * (1 - timeStep);
-        velocity->y += colNormal.y * (float)fabs(velocity->y) * (1 - timeStep);
-        return true;
-    }
-    return false;
+void HitboxMoveAndCollideGrid(NezRect_f *a, NezVec2_f *velocity, NezGridMap *grid){
+    NezRect_f colArea = HitboxExpand(a, velocity);
+    NezRect_f *tiles = (void*){0};
+    int count;
+    HitboxListFromGrid(&colArea, grid, &tiles, &count);
+    HitboxMoveAndCollide(a, velocity, tiles, count);
+    // free allocated RectList memory
+    if(tiles){free(tiles);}
 }
 
 #endif //HITBOX_COLLISIONS_IMPLEMENTATION
