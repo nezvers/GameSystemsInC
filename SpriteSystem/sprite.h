a4c301c133cd24ad9a763b748bbb590a705c3349
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2026-07-08 10:03:43 +0300

    sprite: refactor (#5)
    * sprite: rewrite
    
    * sprite: fix typo
    
    * Sprite: with demo

--- SpriteSystem/sprite.h
+++ SpriteSystem/sprite.h
@@ -1,268 +1,112 @@
-#ifndef NEZ_SPRITE_H
-#define NEZ_SPRITE_H
+#ifndef SPRITE_H
+#define SPRITE_H
 
-#include "raylib.h"
-#include "stdlib.h"
-#include <stdio.h>
+#include <stdint.h>
 
-typedef struct{
-	float image;        // used as timer (moduled to imageCount) and on drawing floored(cast as int)
-	int imageCount;     // number of images in the animation
-	int fps;            // time in seconds for each frame
-	int *imageID;       // array of sprite IDs
-}SpriteAnimation;
+typedef uint32_t u32;
+typedef int32_t  i32;
+typedef float    f32;
 
-typedef struct{
-	int x;
-	int y;
-	int w;                  // individual sprite image width
-	int h;                  // individual sprite image height
-	int xOrigin;		// origin x point against source rectangle
-	int yOrigin;		// origin y point against source rectangle
-	float xScale;		// scale output rectangle width depending on origin x position
-	float yScale;		// scale output rectangle height depending on origin y position
-	int imageCount;		// number of all frames
-	int animation;		// ID for current animation
-	int animationCount;	// animationList size
-	Texture2D *texture;	// texture holding pointer to sprite sheet texture
-	Vector2 *imagePos;	// list of source x&y on the texture
-	SpriteAnimation *animationList;
-}Sprite;
+typedef struct {
+    f32 x;
+    f32 y;
+} vec2;
 
-#ifndef NEZSPAPI
-    #ifdef NEZ_SPRITE_STATIC
-        #define NEZSSAPI static
-    #else
-        #define NEZSSAPI extern
-    #endif
-#endif
+typedef struct {
+    f32 x;
+    f32 y;
+    f32 w;
+    f32 h;
+} rectf;
 
+// Animation frame list, asuming all frames have same region size
+typedef struct {
+    vec2 *data; // Frame position array
+    u32 count;
+    vec2 size; // width & height
+} Frames;
+
+typedef struct {
+    Frames **frames; // Array of Frame pointers
+    u32 count;
+    u32 animation_index;
+    u32 image_index;
+    f32 frame_rate;
+    f32 time;
+} AnimationSet;
+
+
+typedef struct {
+    AnimationSet animation_set;
+    vec2 position;
+    vec2 origin;
+    vec2 offset;
+    vec2 scale;
+    f32 rotation;
+} Sprite;
+
+/* _________________________________________________________________ */
+
+// Remove name mangling for C++
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// Automatically create sprite image collection
-NEZSSAPI Sprite*
-SpriteNewFromSheet(Texture2D *tex, int w, int h, int xO, int yO);
-// Version for predetermined frame position list array
-NEZSSAPI Sprite*
-SpriteNewFromAtlas(Texture2D *tex, int w, int h, int xO, int yO, Vector2 *posList, int posCount);
-// Free allocated memory
-NEZSSAPI void
-SpriteDestroy(Sprite *sprite, bool destroyAnimations);
-// Adds SpriteAnimation to the Sprite and returns animation ID for it
-NEZSSAPI int
-SpriteAddAnimation(Sprite *sprite, SpriteAnimation anim);
-// Adds SpriteAnimation list and returns ID for first anamation in the new list
-NEZSSAPI int
-SpriteAddAnimationList(Sprite *sprite, SpriteAnimation *anim, int animationCount);
-// Create animation from list of sprite image collection
-NEZSSAPI SpriteAnimation
-SpriteAnimationNew(int *imageIDList, int imageCount,  int fps);
-// Free memory for animation
-NEZSSAPI void
-SpriteAnimationDestroy(SpriteAnimation *anim);
-// Animation update, requires delta time (seconds) since last frame. Or (1.0/60.0) for constant time of 60FPS
-NEZSSAPI void
-SpritePlay(Sprite *sprite, int animID, float delta);
-// Animation update that can trigger callback for AnimationFinished with animation ID
-NEZSSAPI void
-SpritePlayWithCallback(Sprite *sprite, int animID, float delta, void (*AnimationFinished)(int));
-// Just draw the sprite
-NEZSSAPI void
-SpriteDraw(Sprite *sprite);
+void ChangeAnimation(AnimationSet *animation_set, u32 new_animation);
+void UpdateAnimation(AnimationSet *animation_set, f32 delta_time);
+void UpdateSprite(Sprite *sprite, f32 delta_time);
+rectf GetAnimationFrame(AnimationSet *animation_set);
+void GetSpriteFrame(Sprite *sprite, rectf *sprite_rect, rectf *texture_rect);
 
 #ifdef __cplusplus
 }
 #endif
-#endif // NEZ_SPRITE_H
 
-//----------------------------------------------------------------------------
+#endif // SPRITE_H
 
-#ifdef NEZ_SPRITE_IMPLEMENTATION
-#undef NEZ_SPRITE_IMPLEMENTATION
-float SpriteAbs(float x){return x>0.0 ? x : -x;}
 
-Sprite*
-SpriteNewFromSheet(Texture2D *tex, int w, int h, int xO, int yO){
-	Sprite *s = (Sprite*)malloc(sizeof(Sprite));
-	*s = (Sprite){0};
-	s->w = w;
-	s->h = h;
-	s->xOrigin = xO;
-	s->yOrigin = yO;
-	s->xScale = 1.0f;
-	s->yScale = 1.0f;
-	s->texture = tex;
-	s->animation = -1;
-	
-	int xFrames = tex->width / w;
-	int yFrames = tex->height / h;
-	s->imageCount = xFrames *yFrames;
-	s->imagePos = (Vector2*)malloc(sizeof(Vector2)*xFrames *yFrames);
-	
-	for(int y=0; y<yFrames; y++){
-		for(int x=0; x<xFrames; x++){
-			s->imagePos[y *xFrames +x] = (Vector2){(float)x*w, (float)y*h};
-		}
-	}
-	
-	return s;
+/* _________________________________________________________________ */
+
+#ifdef SPRITE_IMPLEMENTATION
+#undef SPRITE_IMPLEMENTATION
+
+#include <assert.h>
+
+void ChangeAnimation(AnimationSet *animation_set, u32 new_animation) {
+    assert(new_animation < animation_set->count);
+    animation_set->animation_index = new_animation;
+    animation_set->image_index = 0;
+    animation_set->time = 0;
 }
 
-Sprite*
-SpriteNewFromAtlas(Texture2D *tex, int w, int h, int xO, int yO, Vector2 *posList, int posCount){
-	Sprite *s = (Sprite*)malloc(sizeof(Sprite));
-	*s = (Sprite){0};
-	s->w = w;
-	s->h = h;
-	s->xOrigin = xO;
-	s->yOrigin = yO;
-	s->xScale = 1.0f;
-	s->yScale = 1.0f;
-	s->texture = tex;
-	s->animation = -1;
-	
-	s->imageCount = posCount;
-	s->imagePos = (Vector2*)malloc(sizeof(Vector2) *posCount);
-	
-	for(int i=0; i<posCount; i++){
-		s->imagePos[i] = posList[i];
-	}
-	
-	return s;
+void UpdateAnimation(AnimationSet *animation_set, f32 delta_time) {
+    animation_set->time += delta_time * animation_set->frame_rate;
+    if (animation_set->time < 1) { return; }
+
+    const Frames *frame = animation_set->frames[animation_set->animation_index];
+    const u32 image_count = frame->count;
+    const u32 increment = animation_set->time;
+    animation_set->time -= increment;
+    animation_set->image_index = (animation_set->image_index + increment) % image_count;
 }
 
-void
-SpriteDestroy(Sprite *sprite, bool destroyAnimations){
-	if(destroyAnimations){
-		for(int i=0; i<sprite->animationCount; i++){
-			SpriteAnimationDestroy(&sprite->animationList[i]);
-		}
-	}
-	free(sprite->animationList);
-	free(sprite->imagePos);
-	free(sprite);
+void UpdateSprite(Sprite *sprite, f32 delta_time) {
+    UpdateAnimation(&sprite->animation_set, delta_time);
 }
 
-int
-SpriteAddAnimation(Sprite *sprite, SpriteAnimation anim){
-	if(!anim.imageID){
-        printf("Invalid animation\n");
-        return -1;
-    }
-    
-    SpriteAnimation *newList = (SpriteAnimation*)malloc(sizeof(SpriteAnimation) *(sprite->animationCount +1));
-	for(int i=0; i< sprite->animationCount; i++){
-		newList[i] = sprite->animationList[i];
-	}
-	free(sprite->animationList);
-	sprite->animationList = newList;
-	sprite->animationList[sprite->animationCount] = anim;
-	sprite->animationCount++;
-	return sprite->animationCount -1;
+rectf GetAnimationFrame(AnimationSet *animation_set) {
+    const Frames *frame = animation_set->frames[animation_set->animation_index];
+    const vec2 pos = frame->data[animation_set->image_index];
+    const vec2 size = frame->size;
+    const rectf result = {pos.x, pos.y, size.x, size.y};
+    return result;
 }
 
-int
-SpriteAddAnimationList(Sprite *sprite, SpriteAnimation *anim, int animationCount){
-	SpriteAnimation *newList = (SpriteAnimation*)malloc(sizeof(SpriteAnimation) *(sprite->animationCount +animationCount));
-	//old animations
-	for(int i=0; i< sprite->animationCount; i++){
-		newList[i] = sprite->animationList[i];
-	}
-	//new animations
-	for(int i=0; i< animationCount; i++){
-		newList[sprite->animationCount +i] = anim[i];
-	}
-	free(sprite->animationList);
-	sprite->animationList = newList;
-	sprite->animationCount += animationCount;
-	return sprite->animationCount -animationCount;
+// texture_out = source rectangle from texture
+// sprite_out = destination rectangle on screen
+void GetSpriteFrame(Sprite *sprite, rectf *texture_out, rectf *sprite_out) {
+    *texture_out = GetAnimationFrame(&sprite->animation_set);
+    *sprite_out = (rectf){sprite->position.x, sprite->position.y, texture_out->w, texture_out->h};
 }
 
-
-
-SpriteAnimation
-SpriteAnimationNew(int *imageIDList, int imageCount,  int fps){
-	SpriteAnimation sa = {0};
-	sa.fps = fps;
-	sa.imageCount = imageCount;
-	sa.imageID = (int*)malloc(sizeof(int) *imageCount);
-	for (int i=0; i<imageCount; i++){
-		sa.imageID[i] = imageIDList[i];
-	}
-	return sa;
-}
-
-void
-SpriteAnimationDestroy(SpriteAnimation *anim){
-	free(anim->imageID);
-}
-
-
-void
-SpritePlay(Sprite *sprite, int animID, float delta){
-	if(sprite->animation != animID){
-		sprite->animation = animID;
-		sprite->animationList[animID].image = 0.0f;
-	}
-	else{
-		sprite->animationList[animID].image += delta *sprite->animationList[animID].fps;
-		if(sprite->animationList[animID].image > sprite->animationList[animID].imageCount){
-			sprite->animationList[animID].image -= (float)(int)sprite->animationList[animID].image;
-		}
-	}
-}
-
-void
-SpritePlayWithCallback(Sprite *sprite, int animID, float delta, void (*AnimationFinished)(int)){
-	if(sprite->animation != animID){
-		sprite->animation = animID;
-		sprite->animationList[animID].image = 0.0f;
-	}
-	else{
-		sprite->animationList[animID].image += delta *sprite->animationList[animID].fps;
-		if(sprite->animationList[animID].image > sprite->animationList[animID].imageCount){
-			sprite->animationList[animID].image -= (float)(int)sprite->animationList[animID].image;
-			AnimationFinished(animID);
-		}
-	}
-}
-
-
-void
-SpriteDraw(Sprite *sprite){
-	SpriteAnimation *anim = &sprite->animationList[sprite->animation];
-	int id = anim->imageID[(int)anim->image];
-	Vector2 sourcePos = sprite->imagePos[id];
-	
-	Vector2 offset = (Vector2){sprite->xOrigin *sprite->xScale, sprite->yOrigin *sprite->yScale};
-    
-	
-    float x = sprite->x -offset.x;
-    float y = sprite->y -offset.y;
-    float w = sprite->w *SpriteAbs(sprite->xScale);
-    float h = sprite->h *SpriteAbs(sprite->yScale);
-    
-    
-	Rectangle source = {sourcePos.x, sourcePos.y, (float)sprite->w, (float)sprite->h};
-    if (sprite->xScale < 0.0){
-        source.width = -(float)sprite->w;
-		x -= w;
-    }
-    if (sprite->yScale < 0.0){
-        source.y += sprite->h;
-        source.height *= -1;
-		y -= h;
-    }
-	
-    Rectangle dest = {x, y, w, h};
-    Vector2 origin = (Vector2){ 0.0f,  0.0f};
-    float rotation = 0.0f;
-    
-	DrawTexturePro(*sprite->texture, source, dest, origin, rotation, WHITE);
-}
-
-#endif // NEZ_SPRITE_IMPLEMENTATION
-
-
+#endif
---
+++ SpriteSystem/sprite_raylib.h
@@ -0,0 +1,69 @@
+#ifndef SPRITE_RAYLIB_H
+#define SPRITE_RAYLIB_H
+
+#include "raylib.h"
+#include "sprite.h"
+
+typedef struct {
+    Sprite sprite;
+    Texture2D *texture;
+    Color tint;
+} SpriteRaylib;
+
+// Remove name mangling for C++
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void DrawSpriteRaylib(SpriteRaylib *raylib_sprite);
+void DrawSprite(Sprite *sprite, Texture2D *texture, Color tint);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SPRITE_RAYLIB_H
+
+
+#define SPRITE_RAYLIB_IMPLEMENTATION
+
+#ifdef SPRITE_RAYLIB_IMPLEMENTATION
+#undef SPRITE_RAYLIB_IMPLEMENTATION
+
+#include <math.h>
+
+void DrawSpriteRaylib(SpriteRaylib *raylib_sprite) {
+    DrawSprite(&raylib_sprite->sprite, raylib_sprite->texture, raylib_sprite->tint);
+}
+
+void DrawSprite(Sprite *sprite, Texture2D *texture, Color tint) {
+    rectf target_rect;
+    rectf source_rect;
+    GetSpriteFrame(sprite, &source_rect, &target_rect);
+    target_rect.x = sprite->offset.x;
+    target_rect.y = sprite->offset.y;
+
+    vec2 origin = {sprite->origin.x * abs(sprite->scale.x), sprite->origin.y * abs(sprite->scale.y)};
+
+    if (sprite->scale.x < 0.f) {
+        source_rect.w += -1;
+    }
+
+    if (sprite->scale.y < 0.f) {
+        origin.y = sprite->origin.y * sprite->scale.y - target_rect.h * sprite->scale.y;
+        source_rect.h *= -1;
+    }
+    target_rect.w *= sprite->scale.x;
+    target_rect.h *= sprite->scale.y;
+
+    DrawTexturePro(
+        *texture,
+        *(Rectangle*)& source_rect,
+        *(Rectangle*)& target_rect,
+        *(Vector2*)& origin,
+        sprite->rotation,
+        tint
+    );
+}
+
+#endif // SPRITE_RAYLIB_IMPLEMENTATION
